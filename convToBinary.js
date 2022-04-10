1541329533b0801db1893b8b34fb9f942afbb846
GST-Slava <gstslava@gmail.com>
2022-04-10 18:38:03 +0300

    add 2 sections

---
+++ convToBinary.js
@@ -0,0 +1,11 @@
+toBinary = (n) => parseInt(n.toString(2));
+
+
+//other
+//let toBinary = n => +n.toString(2)
+
+/*
+function toBinary(n){
+  return +n.toString(2);
+}
+*/
--- keepHydrated.js
+++ keepHydrated.js
@@ -4,4 +4,3 @@ let litres = time => Math.floor(time/2);
 // Math.floor()  https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
 
 //others solution
-
