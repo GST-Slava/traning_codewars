77aebefe9ba13b0e84a1ce2b3b5f9b1278376e68
GST-Slava <gstslava@gmail.com>
2022-04-18 20:07:51 +0300

    add 8kyu Fundamentals: Return

---
+++ Fundamentals-Return.js
@@ -0,0 +1,27 @@
+function add(a,b){
+    return a + b
+}//добавить
+function divide(a,b){
+    return a / b
+}//разделить
+function multiply(a,b){
+    return a * b
+}//умножить
+function mod(a,b){
+    return a % b
+}//деление по модулю или остаток от деления
+function exponent(a,b){
+    return a ** b
+}//возведение в степень
+function subt(a,b){
+    return a - b
+}//вычитание
+
+//other
+
+//const add = (a, b) => a + b;
+//const subt = (a, b) => a - b;
+//const divide = (a, b) => a / b;
+//const multiply = (a, b) => a * b;
+//const mod = (a, b) => a % b;
+//const exponent = (a, b) => a ** b;
